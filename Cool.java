7556dafd8b425d30995fcdbfd5fe73708e9145a9
Mason Tolley <fourboystolley@outlook.com>
2020-09-13 16:13:41 -0600

    First commit

---
+++ Cool.java
@@ -0,0 +1,9 @@
+/**
+*This will be a cool program someday
+*@author Mason Tolley
+*/
+public class Cool {
+	public static void main(String[] args) {
+		System.out.println("Coolest thing ever!");
+	}
+}
